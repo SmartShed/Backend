9dc7bde0fa4a4093e21b4c451d9c4a9221e41f10
Parth Sali <parthsali04@gmail.com>
2023-11-14 20:58:00 +0530

    update section schema

--- models/SectionData.js
+++ models/SectionData.js
@@ -5,7 +5,7 @@ const sectionSchema = new Schema({
     sectionID: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     logo: { type: String }, // Assuming logo is a file path or URL
-    forms: [{ type: Schema.Types.ObjectId, ref: 'Form' }],
+    forms: [{ type: Number }],
 });
 
 

63aedb0a7a13b71801cd7075cab05e16b5da8596
Parth Sali <parthsali04@gmail.com>
2023-11-14 20:58:33 +0530

    update formdata schema

--- models/FormData.js
+++ models/FormData.js
@@ -5,8 +5,8 @@ const formSchema = mongoose.Schema({
     formID: { type: String, required: true, unique: true },
     title: { type: String, required: true },
     description: { type: String },
-    questions: [{ type: Schema.Types.ObjectId, ref: 'Question' }],
-    sectionID: [{ type: Schema.Types.ObjectId, ref: 'Section' }],
+    questions: [{ type: Number }],
+    sectionID: [{ type: Number }],
 });
 
 module.exports = mongoose.model('FormData', formSchema);

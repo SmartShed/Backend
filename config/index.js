95cdc999e3fdf85a1be8743bde8f6bf485b9c4d2
Parth Sali <parthsali04@gmail.com>
2023-12-01 23:59:25 +0530

    Update SendMail Helper

--- config/index.js
+++ config/index.js
@@ -4,9 +4,13 @@ dotenv.config();
 const APP_PORT = process.env.APP_PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
 
 module.exports = {
   APP_PORT,
   MONGO_URI,
   JWT_SECRET,
+  EMAIL,
+  PASSWORD,
 };
---
+++ helpers/index.js
@@ -0,0 +1,5 @@
+const sendMail = require("../helpers/sendMail");
+
+module.exports = {
+    sendMail,
+};

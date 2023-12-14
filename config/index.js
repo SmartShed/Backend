7bf591cf89548fabc0c115d38834f70733f33287
GopalSaraf <sarafgopal37@gmail.com>
2023-12-14 15:02:08 +0530

    AWS EC2 HTTPS SSL Cert added with extra PORT

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const APP_PORT = process.env.APP_PORT;
+const APP_HTTPS_PORT = process.env.APP_HTTPS_PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 const EMAIL = process.env.EMAIL;
@@ -9,6 +10,7 @@ const PASSWORD = process.env.PASSWORD;
 
 module.exports = {
   APP_PORT,
+  APP_HTTPS_PORT,
   MONGO_URI,
   JWT_SECRET,
   EMAIL,
--- index.js
+++ index.js
@@ -2,18 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const fs = require("fs");
+const https = require("https");
+
 app.use(cors());
 
-const QuestionData = require("./models/QuestionData");
-const SectionData = require("./models/SectionData");
-const FormData = require("./models/FormData");
-
-const Form = require("./models/Form");
-
-const SubForm = require("./models/SubForm");
-const Question = require("./models/Question");
-
-const { APP_PORT } = require("./config");
+const { APP_PORT, APP_HTTPS_PORT } = require("./config");
 const connectDB = require("./config/database");
 
 const {
@@ -28,107 +22,28 @@ const {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Database
-// username - smartshedteam
-// password - smartshedteam
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get("/", (req, res) => res.send("Welcome to SmartShed API"));
 
 app.use("/api/forms", formRoutes);
 app.use("/api/auth", userRoutes);
 app.use("/api/workers", workerRoutes);
 app.use("/api/sections", sectionRoutes);
 app.use("/api/create", smartShedRoutes);
-
 app.use("/api", others);
 
-// app.get("/api/getquestions", async (req, res) => {
-//   const questions = await QuestionData.find();
+app.listen(APP_PORT, () =>
+  console.log(`Server is running on port ${APP_PORT}`)
+);
 
-//   res.status(200).json(questions);
-// });
+const key = fs.readFileSync("private.key");
+const cert = fs.readFileSync("certificate.crt");
 
-// app.post("/api/addquestion", async (req, res) => {
-//   const question = req.body;
+const cred = { key: key, cert: cert };
 
-//   try {
-//     const que = await QuestionData.findOne({ questionID: question.questionID });
-//     console.log(que);
-//     if (que) {
-//       res.status(400).send("Question ID already exists");
-//       return;
-//     }
+const httpsServer = https.createServer(cred, app);
 
-//     const newQuestion = new QuestionData(question);
-
-//     newQuestion
-//       .save()
-//       .then(() => res.status(200).send("Question added"))
-//       .catch((err) => res.status(400).send(err));
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// app.post("/api/addsection", async (req, res) => {
-//   const section = req.body;
-
-//   try {
-//     const sec = await SectionData.findOne({ sectionID: section.sectionID });
-
-//     if (sec) {
-//       res.status(400).send("Section ID already exists");
-//       return;
-//     }
-
-//     const newSection = new SectionData(section);
-
-//     newSection
-//       .save()
-//       .then(() => res.status(200).send("Section added"))
-//       .catch((err) => res.status(400).send(err));
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// app.post("/api/addform", async (req, res) => {
-//   const form = req.body;
-
-//   console.log("form", form);
-
-//   try {
-//     const tempForm = await FormData.findOne({ formID: form.formID });
-
-//     console.log("tempForm", tempForm);
-
-//     if (tempForm) {
-//       res.status(400).send("Form ID already exists");
-//       return;
-//     }
-
-//     const newForm = new FormData(form);
-
-//     console.log("newForm", newForm);
-//     await newForm.save();
-
-//     res.status(200).json({ message: "Form added" });
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// app.get("/api/clear", async (req, res) => {
-//   await Question.deleteMany({});
-//   await SubForm.deleteMany({});
-//   await Form.deleteMany({});
-
-//   res.status(200).send("Cleared");
-// });
-
-app.listen(APP_PORT, () => {
-  console.log(`Server is running on port ${APP_PORT}`);
+httpsServer.listen(APP_HTTPS_PORT, () => {
+  console.log(`HTTPS Server running on port ${APP_HTTPS_PORT}`);
 });

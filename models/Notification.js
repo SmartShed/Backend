4dcc5641ad33238cd7e7d821cb7251c48dfe7043
Parth Sali <parthsali04@gmail.com>
2023-12-21 12:30:13 +0530

    Modify Notification Schema

--- models/Notification.js
+++ models/Notification.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-    type: { type: String, required: true },
+    contentEnglish: { type: String, required: true },
+    contentHindi: { type: String, required: true },
     isRead: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
+    formId: { type: mongoose.Schema.Types.ObjectId, ref: 'Form' },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
-module.exports = mongoose.model('Notification', notificationSchema);
+module.exports = mongoose.model('Notification', notificationSchema);    
